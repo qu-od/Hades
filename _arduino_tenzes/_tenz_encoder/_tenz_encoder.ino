b3e86d51422bef7e6e30341cbc90ddbb222ec3a6
Андрей <andree.nv2009@yandex.ru>
2021-04-05 11:07:03 +0300

    bytes communication

--- _arduino_tenzes/_tenz_encoder/_tenz_encoder.ino
+++ _arduino_tenzes/_tenz_encoder/_tenz_encoder.ino
@@ -8,96 +8,73 @@ void setup() {
 
   Serial.begin(19200);
   tenz.begin(dt,sck);
-  
-  Serial.println("Показания датчика ДО калибровки и тарирования");
-  
-  Serial.print("Показания датчика по 1-ому измерению: \t\t");
-  Serial.println(tenz.read());
 
-  Serial.print("Показания датчика по 10-ти измерениям: \t\t");
-  Serial.println(tenz.read_average(10));
-
-  Serial.print("Масса тары: \t\t");
-  Serial.println(tenz.get_offset());
-
-  Serial.print("Калибровочный коэффициент: \t\t");
-  Serial.println(tenz.get_scale());
-
-  Serial.println("Установите в течение 5 секунд тару на тензодатчик");
-  delay(5000);
-  tenz.tare(10);
-  tenz.set_scale();
-
-  Serial.println("Показания датчика ПОСЛЕ калибровки и тарирования");
-  
-  Serial.print("Показания датчика по 1-ому измерению: \t\t");
-  Serial.println(tenz.read());
-
-  Serial.print("Показания датчика по 10-ти измерениям: \t\t");
-  Serial.println(tenz.read_average(10));
-
-  Serial.print("Масса тары: \t\t");
-  Serial.println(tenz.get_offset());
-
-  Serial.print("Калибровочный коэффициент: \t\t");
-  Serial.println(tenz.get_scale());
-
-  Serial.print("Показания датчика с учетом веса тары: \t\t");
+  /*Serial.print("Показания датчика с учетом веса тары: \t\t");
   Serial.println(tenz.get_value(10));
 
   Serial.print("Показания датчика с учетом веса тары и с учетом калибровочного коэффициента: \t\t");
-  Serial.println(tenz.get_units(10));
+  Serial.println(tenz.get_units(10));*/
   
-
 }
 
 void loop() {
-  delay(1000);
-  //определение и обнуление
-  int command = '0';
-  String response("");
+  delay(300);
+  
+  int command_len_in_bytes = 6;
+  char command_buffer[command_len_in_bytes];
+  struct Command{ //7 bytes
+    char header; // to recognize a command header must be 42
+    //header is kinda key to acsess 
+    char number; // uint8 command number
+    //unsigned int data_field_size; // uint8 data field size
+    int data_value; // data value (int - 4 bytes long)
+    };
+  int response_len_in_bytes = 6;
+  struct Response{
+    char header; // to recognize a response header must be 42
+    char error_code; // 0 means "no error" (1 byte for char)
+    float data_value; // 4 bytes for float
+    };
 
-  //обработка команды (если она пришла)
-  if(Serial.available() > 0)
-  {
-      command = Serial.read(); // Reads the data from the serial port
-      response += String("Command recieved!");
-
-    if (command == '0') { // "make tare" command
-      tenz.tare();
-      response += String(" Tenz tared.");
-    }
-
-    if (command == '1') { // "get value" command supposed to 
-      int value = tenz.get_value(5);
-      response += (String(", ") + String(value));
-    }
-
-    if (command == '2') { // "set scale" command
-      float test_scale = 32;
-      tenz.set_scale(test_scale);
-      response += (String(" New scale = ") + String(test_scale));
-    }
-
-    if (command == '3') { // "get units" command
-      int units = tenz.get_units(5);
-      response += (String(", ") + String(units));
-    }
-
-    if (command == '9') { // "send info" command
-      int offset = tenz.get_offset();
-      int scale = tenz.get_scale();
-      response += (String(" Offset = ") + String(offset) + String(",  Scale = ") +  String(scale));
-    } 
-
-    //if (command > 2) {
-    //  response = String('Wrong command');
-    //}
-
-    //Serial.println(command);
-    Serial.println(response);
+  if (Serial.available() > 0){ 
+    Command command;
+    Response response;
+    response.header = 42;
+    int number_of_bytes_read = Serial.readBytes(command_buffer, Serial.available());
+    if (number_of_bytes_read == command_len_in_bytes){
+      response.error_code = 100; // clear with initial error_code 
+      response.data_value = 0; // clear data
+      memcpy(&command, command_buffer, number_of_bytes_read); //unpack from bytes
+      if (command.header == 42){
+        if (command.number == 0){           // tare()
+          tenz.tare(1);
+          response.error_code = 0;
+          }
+        else if (command.number == 1){      // get_value()
+          float value = tenz.get_value(1);
+          response.data_value = value;
+          response.error_code = 0;
+          }
+        else if (command.number == 2){      // set_scale()
+          tenz.set_scale(command.data_value); // NEED TESTING!
+          response.error_code = 0;
+          }
+        else if (command.number == 3){      // get_units()
+          float units = tenz.get_units(1);
+          response.data_value = units;
+          response.error_code = 0;
+          }
+        else {
+          response.error_code = 3; // Wrong command number
+          }
+        }
+      else {response.error_code = 2;} // Wrong command header
+      }
+    else {response.error_code = 1;} // Wrong command size
+    char bytes_response[sizeof(response)];
+    memcpy(bytes_response, &response, sizeof(response)); //pack to bytes
+    Serial.write(bytes_response, response_len_in_bytes);
   }
+  
+
 }
-
-
-
--- _python_client/bt_serial.py
+++ _python_client/bt_serial.py
@@ -30,6 +30,7 @@ class TenzSerialPort(object):
         self._number: int = comport_number
         self._baudrate: int = baudrate
         self.name: Optional[str] = name
+        self.header: int = 42 #key for tenz commands
 
     def __str__(self): return f"PORT COM-{self._number}" #to ease debug messages
     
@@ -38,7 +39,12 @@ class TenzSerialPort(object):
             self._ser = serial.Serial(f'COM{self._number}') #self means "COM1" for COM1 port
             print(f'{self} opened ***\\')
         except serial.serialutil.SerialException:
-            print(f'{self} was not opened. It was not found most probably')
+            print(f'{self} was not opened! It was not found most probably')
+
+    def flush_input(self):
+        if self._ser:
+            self._ser.flushInput()
+            print('Input flushed')
     
     def close(self):
         if self._ser: 
@@ -50,37 +56,63 @@ class TenzSerialPort(object):
             return
 
     def write_char(self, char: str): #string must contain only one symbol
-        self._ser.write(char.encode("utf-8"))
+        if self._ser: self._ser.write(char.encode("utf-8"))
         print(f'--- Char IN {self} --- : {char}')
+
+    def write_bytes(self, data: bytes):
+        self._ser.write(data)
+        print(f'--- Line IN {self} --- : {data}')
 
     def write_line(self, line: str):
         self._ser.write((line + '\n').encode("utf-8"))
         print(f'--- Line IN {self} --- : {line}')
+
+    def read_bytes(self, bytes_number) -> bytes:
+        data: bytes = self._ser.read(size = bytes_number)
+        print(f'--- FROM {self} --- :', data)
+        return data
 
     def read_line(self) -> str:
         line: str = self._ser.readline().decode("utf-8")
         print(f'--- FROM {self} --- :', line[:-1])
         return line
 
-    def exec_command(self, command_number: int):
-        tenz_port.write_char(str(command_number))
-        return self.read_line()
+    def exec_command(
+            self, command_number: int, 
+            data_value: int = 0):
+        #now size of data_value is 4 bytes (signed int)
+        #data_value must be an optional field to make most commands 3 times shorter 
+        self.write_bytes(
+            struct.pack('>2Bi', self.header, command_number, data_value)
+        )
+        response_header: int
+        error_code: int
+        response_data_value: float
+        response_header, error_code, response_data_value = \
+
+            struct.unpack('>2Bi', self.read_bytes(6)) #NEED TESTING!
+            
+        #also prints response in console
+        if error_code != 0: 
+            print(f'ERROR NUMBER {error_code} OCCURED ON ARDUINO')
+            return
+        elif error_code == 0: 
+            return response_data_value
 
     def tare(self):
         self.exec_command(0)
 
     def get_value(self) -> float:
-        time.sleep(3)
-        response = self.exec_command(1)
-        return float(response.split(', ')[-1])
+        # time.sleep(3)
+        value: float = self.exec_command(1)
+        return value
 
-    def set_scale(self, scale: float):
-        self.exec_command(2)
-        #SEND SCALE HERE!
+    def set_scale(self, scale: int):
+        self.exec_command(2, scale)
 
     def get_units(self) -> float:
-        response = self.exec_command(3)
-        return float(response.split(', ')[-1])
+        units: float = self.exec_command(3)
+        return units
 
 
 
@@ -105,32 +137,40 @@ class TenzPorts(object):
 
 
 #-----------------------------------MAIN FOR TESTING-------------------------------------
-if __name__ == '__main__':
+def calibration_test():
     #MAKE CONTEXT MANAGER FOR WirelessSerialPort
 
-    tenz_port = TenzSerialPort(11)
+    tenz_port = TenzSerialPort(5)
     tenz_port.open()
-
-
+    # tenz_port.flush_input()
     tenz_port.tare() #TARE WITH NO LOAD
-    
-    weights = [1, 2, 3, 4] #in kilos
+    # weights = [1, 2, 3, 4] #in kilos
+    weights = [1, 2] #in kilos
     scales = [  
         tenz_port.get_value() / weights[i] #TRY A BUNCH OF LOADS (better be 5 to 10):
         for i in range(len(weights))
         ]
     mean_scale: float = sum(scales) / len(scales)
-
     tenz_port.set_scale(mean_scale) #SET_SCALE
-
     data_in_kilos = [
         tenz_port.get_units() #GET_UNITS in kilos
         for _ in range (100)
       ]  
-    
     # tenz_port.exec_command('100') #wrong command attempt
+    tenz_port.close()
+
+if __name__ == '__main__':
+    # calibration_test()
+    tenz_port = TenzSerialPort(5)
+    tenz_port.open()
+
+    tenz_port.tare()
+    print(tenz_port.get_value())
+    tenz_port.set_scale(15)
+    print(tenz_port.get_units())
 
     tenz_port.close()
+
 
 
 
